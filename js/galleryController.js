a7370be0361eddb2934f2bd5702e73d13f3d9711
Shachar Ron Zohar <Rzshachar@gmail.com>
2022-02-16 13:08:13 +0200

    Should've commited last time idk what happend

--- js/galleryController.js
+++ js/galleryController.js
@@ -3,17 +3,12 @@
 function renderGallery() {
     // const imgs = 
     getImgs().forEach(img => {
-            // elImg.src = `assets/meme-imgs/${img.id}.jpg`
-            const elGallContainer = document.querySelector('.main-gallery-container')
-            elGallContainer.innerHTML +=
-                `<div class="img-container gallery-article" onclick="onImgClicked">
+        const elGallContainer = document.querySelector('.main-gallery-container')
+        elGallContainer.innerHTML +=
+            `<div class="img-container gallery-article" onclick="onImgClicked">
             <img src="assets/meme-imgs/${img.id}.jpg"/></div>`
-                // elImg.onload = renderGalleryItem.bind(event, img.id)
-                // console.log('elImg', elImg)
 
-        })
-        // console.log('imgs', imgs)
-
+    })
 }
 
 function onImgClicked() {
