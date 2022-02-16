5f5d9421292204804f7a6fde41861e240a75bc18
Shachar Ron Zohar <Rzshachar@gmail.com>
2022-02-16 10:27:01 +0200

    I don't know why it didn't commit this aswell the last time

--- js/memeController.js
+++ js/memeController.js
@@ -1,1 +1,37 @@
 'use strict'
+
+
+function init() {
+    const elCanvas = document.querySelector('#main-canvas')
+    const canvasCtx = document.querySelector('#main-canvas').getContext('2d')
+    _addEventListeners(elCanvas, canvasCtx)
+    resizeCanvas(elCanvas)
+}
+
+
+function renderCanvas(elCanvas, canvasCtx) {
+    canvasCtx.save()
+    canvasCtx.fillStyle = "red"
+    canvasCtx.fillRect(0, 0, elCanvas.width, elCanvas.height)
+        // renderCircle()
+    canvasCtx.restore()
+}
+
+function resizeCanvas(elCanvas) {
+    // console.log('elCanvas', elCanvas)
+
+    const elCanvasContainer = document.querySelector('.main-canvas-container')
+    elCanvas.height = elCanvasContainer.offsetHeight
+    elCanvas.width = elCanvasContainer.offsetWidth
+}
+
+function _addEventListeners(elCanvas, canvasCtx) {
+    _addCanvasResizeListener(elCanvas, canvasCtx)
+}
+
+function _addCanvasResizeListener(elCanvas, canvasCtx) {
+    window.addEventListener('resize', () => {
+        resizeCanvas(elCanvas)
+        renderCanvas(elCanvas, canvasCtx)
+    })
+}
